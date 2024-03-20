44a46cff0014da2bc7cf1dc8066ed890538d282c
AleksandrDruk <aleksandr.druk.ru@gmail.com>
2024-03-20 20:01:39 +0300

    upd

--- main.js
+++
@@ -1,9 +0,0 @@
-//Реализация preloader
-window.onload = function () {
-  var preloader = document.getElementById("heart-box");
-  preloader.classList.add("fadeOut");
-
-  setTimeout(function () {
-    preloader.style.display = "none";
-  }, 800);
-};
